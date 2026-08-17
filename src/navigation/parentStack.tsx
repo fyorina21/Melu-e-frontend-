f983d7298adf3f1c9989b57da7f983d2b929c8b9
Hanania Meseret <hananiyameseret@gmail.com>
2026-08-17 08:01:50 +0300

    add parent navigation stack

---
+++ src/navigation/parentStack.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { ParentStackParamList } from '../types';
+import ParentDashboardScreen from '../screens/parent/ParentDashboardScreen';
+import ChildProgressScreen from '../screens/parent/ChildProgressScreen';
+import HomeObservationLogScreen from '../screens/parent/HomeObservationLogScreen';
+import ParentCommunicationScreen from '../screens/parent/ParentCommunicationScreen';
+import ParentReportsScreen from '../screens/parent/ParentReportsScreen';
+import ParentNotificationsScreen from '../screens/notifications/ParentNotificationsScreen';
+
+const Stack = createNativeStackNavigator<ParentStackParamList>();
+
+export default function ParentStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="ParentDashboard" component={ParentDashboardScreen} />
+      <Stack.Screen name="ChildProgress" component={ChildProgressScreen} />
+      <Stack.Screen name="HomeObservationLog" component={HomeObservationLogScreen} />
+      <Stack.Screen name="ParentCommunication" component={ParentCommunicationScreen} />
+      <Stack.Screen name="ParentReports" component={ParentReportsScreen} />
+      <Stack.Screen name="Notifications" component={ParentNotificationsScreen} />
+    </Stack.Navigator>
+  );
+}
